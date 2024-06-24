d7270451c62fa11a82b1e9b3b4e7855a98977985
Jorge Acosta <Jorge.acosta@convergetp.com>
2024-06-24 16:23:26 -0400

    improve shoot function, add fzf keys to fish config, show lock mode on zellij

--- dot_config/private_fish/conf.d/abbr.fish
+++ dot_config/private_fish/conf.d/abbr.fish
@@ -14,8 +14,6 @@ abbr cat "bat -pp"
 abbr ff "fd --type file"
 abbr fd "fd --type dir"
 
-abbr shoot "procs | fzf | awk '{print \$1}' | xargs kill -9"
-
 abbr md "fzf --query .md| xargs glow -p"
 abbr vmd "fzf --query .md --preview=\"glow {-1}\" --bind 'enter:become(nvim {+}),ctrl-y:execute-silent(echo {} | wl-copy -n)+abort'"
 
--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -15,4 +15,5 @@ if status is-interactive
     --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
     --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
     --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
+    fzf_key_bindings
 end
