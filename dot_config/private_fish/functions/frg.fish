b56b51e18bc3dffa21f73a79c91ec09215da13c1
Jorge Acosta <Jorge.acosta@convergetp.com>
2024-07-09 09:26:50 -0400

    fixing theme config for bat and replacing g for f for frg function so zellij can still use g for locking

--- dot_config/private_fish/functions/frg.fish
+++ dot_config/private_fish/functions/frg.fish
@@ -6,14 +6,14 @@ function frg --description "rg tui built with fzf and bat"
             --query "$argv" \
             --bind "start:reload:$RG_PREFIX {q}" \
             --bind "change:reload:sleep 0.1; $RG_PREFIX {q} || true" \
-            --bind 'ctrl-g:transform:[[ ! $FZF_PROMPT =~ fzf ]] &&
+            --bind 'ctrl-f:transform:[[ ! $FZF_PROMPT =~ fzf ]] &&
               echo "change-prompt(fzf> )+enable-search+clear-query" ||
               echo "change-prompt(rgrep> )+enable-search+clear-query"' \
             --color "hl:-1:underline,hl+:-1:underline:reverse" \
             --delimiter ':' \
             --preview "bat --color=always {1} --highlight-line {2}" \
             --prompt 'ripgrep> ' \
-            --header 'CTRL-g: Switch between fzf/rgrep' \
+            --header 'CTRL-f: Switch between fzf/rgrep' \
             --preview-window 'up,60%,border-bottom,+{2}+3/3,~3' \
             --bind "enter:become(nvim +{2} {1})"
 end
