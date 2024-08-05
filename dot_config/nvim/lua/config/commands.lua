73ce023c9b8e94caf9360d48393410c472438485
Jorge Acosta <Jorge.acosta@convergetp.com>
2024-08-05 13:59:10 -0400

    Update .config/nvim/lua/config/commands.lua

--- dot_config/nvim/lua/config/commands.lua
+++ dot_config/nvim/lua/config/commands.lua
@@ -5,9 +5,47 @@
 -- License: GPLv3
 --
 --- Code:
+local home_dir = os.getenv("HOME")
+package.path = package.path .. home_dir .. "/.config/nvim/lua/config/?.lua;"
 
 vim.api.nvim_create_user_command("SudoWrite", function()
   require("sudo_write").write()
 end, { desc = "Write file with sudo permissions" })
 
---- commands.lua ends here
+-- local neorg = require("neorg.core")
+-- local dirman = neorg.modules.get_module("core.dirman")
+-- if dirman then
+--   Current_workspace = dirman.get_current_workspace()[2]
+-- end
+
+vim.api.nvim_create_user_command("Notdo", function()
+  require("telescope.builtin").grep_string({
+    search = [[^\s*(-|~)+.\(.\)]],
+    use_regex = true,
+    -- search_dirs = { tostring(Current_workspace) },
+    search_dirs = { tostring("~/neorg") },
+    prompt_title = "TODOs",
+  })
+end, { desc = "Search for todos/tasks" })
+
+vim.api.nvim_create_user_command("Obtodo", function()
+  require("telescope.builtin").grep_string({
+    search = [[^\s*(-|~)+.\(.\)]],
+    use_regex = true,
+    search_dirs = { tostring("~/Documents/Obsidian/Obsidian") },
+    prompt_title = "TODOs",
+  })
+end, { desc = "Search for Obsidian todos/tasks" })
+
+vim.api.nvim_create_user_command("Sc", function()
+  require("lualine").setup({
+    options = {
+      disabled_filetypes = {
+        statusline = { "scratch" },
+        winbar = { "scratch" },
+        "scratch",
+      },
+    },
+  })
+  vim.cmd("vsplit ~/.scratch.scratch")
+end, { desc = "Create and set a scratchpad" })
