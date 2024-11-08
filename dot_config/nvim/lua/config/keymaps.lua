6ce316598ac39fc6719bbbae870005e47456dfc4
Jorge Acosta <Jorge.acosta@convergetp.com>
2024-11-08 11:50:07 -0500

    Update .config/nvim/lua/config/keymaps.lua

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -8,7 +8,12 @@ vim.keymap.set(
   { noremap = true, silent = true, desc = "Resume telescope" }
 )
 vim.keymap.set("n", "Y", "y$", { desc = "Yank to end of line" })
-vim.keymap.set("n", "<leader>?", "<cmd>Telescope keymaps<CR>", { desc = "Show keymaps" })
+vim.keymap.set(
+  "n",
+  "<leader>?",
+  "<cmd>Telescope keymaps<CR>",
+  { desc = "Show keymaps" }
+)
 vim.keymap.set(
   "n",
   "<leader>om",
@@ -16,4 +21,24 @@ vim.keymap.set(
   { desc = "Preview [m]arkdown on new pane" }
 )
 
-vim.keymap.set("x", "<leader>te", [["_dP]], { desc = "Yank and paste properly" })
+vim.keymap.set(
+  "x",
+  "<leader>te",
+  [["_dP]],
+  { desc = "Yank and paste properly" }
+)
+
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "scroll up and center" })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "scroll down and center" })
+vim.keymap.set("n", "n", "nzzzv", { desc = "keep cursor centered" })
+vim.keymap.set("n", "N", "Nzzzv", { desc = "keep cursor centered" })
+vim.keymap.set(
+  "n",
+  "<BS>",
+  "db",
+  { desc = "Move to the first non-blank character" }
+)
+
+vim.keymap.set("i", "<c-y>", function()
+  require("telescope.builtin").registers()
+end, { remap = true, silent = false, desc = "paste register i insert mode" })
