0e3503875af38630b092ee85672ed75781cefebd
Jorge Acosta <Jorge.acosta@convergetp.com>
2025-01-09 12:10:56 -0500

    Update .config/fish/functions/shoot.fish

--- dot_config/private_fish/functions/shoot.fish
+++ dot_config/private_fish/functions/shoot.fish
@@ -1,3 +1,3 @@
 function shoot --description "Search for a process ank kill it"
-    procs | fzf -m --query "$argv" | awk '{print $1}' | xargs kill -9
+    procs | fzf -m --query "$argv" --border | awk '{print $1}' | xargs kill -9
 end
