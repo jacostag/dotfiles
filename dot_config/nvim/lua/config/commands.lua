30fe5cf2602555033387bdbff876422ba605ad1a
Jorge Acosta <Jorge.acosta@convergetp.com>
2025-01-09 12:12:20 -0500

    Update .config/nvim/lua/config/commands.lua

--- dot_config/nvim/lua/config/commands.lua
+++ dot_config/nvim/lua/config/commands.lua
@@ -38,6 +38,21 @@ vim.api.nvim_create_user_command("Obtodo", function()
 end, { desc = "Search for Obsidian todos/tasks" })
 
 vim.api.nvim_create_user_command("Sc", function()
+  -- local bufnr = vim.api.nvim_create_buf(false, true) -- Create a scratch buffer
+  -- vim.bo[bufnr].buftype = "nofile" -- Set buffer type to "nofile"
+  -- vim.bo[bufnr].readonly = false -- Set read-only to false
+  -- vim.bo[bufnr].swapfile = false -- Disable swap file
+  -- vim.bo[bufnr].bufhidden = "" -- Set buffer hidden state to empty
+  -- vim.bo[bufnr].buflisted = true -- Add buffer to buffer list
+  --vim.api.nvim_buf_set_name(bufnr, "Scratch Buffer")
+  -- local win_id = vim.api.nvim_get_current_win()
+  -- local orientation = "horizontal" -- or "vertical" for vertical split
+  -- vim.bo.nvim_command(string.format("wincmd %s", orientation))
+
+  -- Move the cursor to the new scratch buffer
+  -- vim.api.nvim_win_set_cursor(win_id, { 0, 0 })
+
+  --  vim.api.nvim_buf_set_name(bufnr, "*scratch*")
   require("lualine").setup({
     options = {
       disabled_filetypes = {
@@ -48,5 +63,14 @@ vim.api.nvim_create_user_command("Sc", function()
     },
   })
   vim.cmd("vsplit ~/.scratch.scratch")
-  vim.cmd("setlocal buftype=nofile bufhidden=hide noswapfile")
+  vim.cmd("setlocal buftype=nofile bufhidden=wipe noswapfile")
 end, { desc = "Create and set a scratchpad" })
+
+-- Map 'q' to close the buffer in this window
+-- vim.api.nvim_buf_set_keymap(
+--   buf,
+--   "n",
+--   "q",
+--   ":q!<CR>",
+--   { noremap = true, silent = true }
+-- )
