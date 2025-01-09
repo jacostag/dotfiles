4906a961325371fe4058ced780e9f1afc64fe8eb
Jorge Acosta <Jorge.acosta@convergetp.com>
2025-01-09 12:10:13 -0500

    Update .config/fish/functions/fdd.fish

--- dot_config/private_fish/functions/fdd.fish
+++ dot_config/private_fish/functions/fdd.fish
@@ -1,6 +1,7 @@
 function fdd --description "finding directories"
     fd --type directory |
         fzf --prompt 'Directory> ' \
+            --border \
             --bind 'enter:become(nvim {+}),ctrl-y:execute-silent(echo {} | wl-copy -n)+abort' \
-            --preview "eza -T {}"
+            --preview "eza --icons=always --color=always -T {}"
 end
