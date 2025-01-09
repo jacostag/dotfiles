0f875a3461c3061bd7153a7a4b331372ecc54ecf
Jorge Acosta <Jorge.acosta@convergetp.com>
2025-01-09 12:10:43 -0500

    Update .config/fish/functions/frg.fish

--- dot_config/private_fish/functions/frg.fish
+++ dot_config/private_fish/functions/frg.fish
@@ -1,15 +1,18 @@
 function frg --description "rg tui built with fzf and bat"
-    set RG_PREFIX "rg --smart-case --column --color=always --line-number --no-heading"
-    rg --smart-case --column --color=always --line-number --no-heading "$argv" |
+    #set RG_PREFIX "rg --smart-case --column --color=always --line-number --no-heading"
+    set RG_PREFIX "rg --smart-case --column --color=always --no-heading"
+    rg --smart-case --color=always --no-heading "$argv" |
+        #rg --smart-case --column --color=always --line-number --no-heading "$argv" |
         fzf --ansi \
             --multi \
             --disabled \
             --query "$argv" \
+            --border \
             --bind "start:reload:$RG_PREFIX {q}" \
             --bind "change:reload:sleep 0.1; $RG_PREFIX {q} || true" \
             --color "hl:-1:underline,hl+:-1:underline:reverse" \
             --delimiter ':' \
-            --preview "bat --color=always {1} --highlight-line {2}" \
+            --preview "bat -p --color=always {1} --highlight-line {2}" \
             --prompt 'ripgrep> ' \
             --preview-window 'up,60%,border-bottom,+{2}+3/3,~3' \
             --bind "enter:become(nvim {1} +{2}),ctrl-y:execute-silent(echo {} | wl-copy -n)+abort"
