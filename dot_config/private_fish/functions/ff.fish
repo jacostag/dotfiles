a707eb24c83f62a9ccee95a9f4868c2ad7701b8f
Jorge Acosta <Jorge.acosta@convergetp.com>
2025-01-09 12:10:23 -0500

    Update .config/fish/functions/ff.fish

--- dot_config/private_fish/functions/ff.fish
+++ dot_config/private_fish/functions/ff.fish
@@ -2,6 +2,7 @@ function ff --description "finding files"
     fd --type file |
         fzf --prompt 'Files> ' \
             --multi \
+            --border \
             --bind 'enter:become(nvim {+}),ctrl-y:execute-silent(echo {} | wl-copy -n)+abort' \
-            --preview "bat --color=always {}"
+            --preview "bat -p --color=always {}"
 end
