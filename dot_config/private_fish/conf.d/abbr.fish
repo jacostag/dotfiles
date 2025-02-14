1e239d65cc53d49345b4e1620b48bb443dc54a4b
Jorge Acosta <Jorge.acosta@convergetp.com>
2025-02-14 09:47:08 -0500

    Update .config/fish/conf.d/abbr.fish

--- dot_config/private_fish/conf.d/abbr.fish
+++ dot_config/private_fish/conf.d/abbr.fish
@@ -23,7 +23,10 @@ abbr scrcpyAudio 'scrcpy --tcpip=192.168.86.26 --no-video'
 abbr scrcpyCtrl 'scrcpy --tcpip=192.168.86.26 --turn-screen-off'
 abbr scrcpyScreen 'scrcpy --tcpip=192.168.86.26 --turn-screen-off --new-display'
 
-abbr reap 'set -gx GDK_SCALE 2; pw-jack -s 48000 -p 256 reaper -new'
+abbr reap 'set -gx GDK_SCALE 1.75; pw-jack -s 48000 -p 256 reaper -new'
+
+abbr dndon 'gsettings set org.gnome.desktop.notifications show-banners false'
+abbr dndoff 'gsettings set org.gnome.desktop.notifications show-banners true'
 
 abbr sshgem 'ssh kiosk@gemini.circumlunar.space'
 
@@ -32,3 +35,7 @@ abbr vichez 'nvim $HOME/.local/share/chezmoi/'
 abbr u2mac '/usr/bin/ddcutil --noverify setvcp xe7 xff00' #switch to mac display/auto-usb
 
 abbr s2mac '/usr/bin/ddcutil setvcp 60 0x1b' #switch usb to mac
+
+abbr delsnaps 'sudo snapper --config root --csvout list --columns number | rg -v "0|number" | xargs -I {} sudo snapper delete -s {}'
+
+#bash -c 'gsettings set org.gnome.desktop.notifications show-banners $(if [ "$(gsettings get org.gnome.desktop.notifications show-banners)" = "true" ]; then echo "false"; else echo "true"; fi)'
