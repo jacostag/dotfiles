e331fd2ad7b3f7ab40a8b8005a233022f28f5bbc
Jorge Acosta <Jorge.acosta@convergetp.com>
2025-04-02 15:50:34 -0400

    Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,7 +1,8 @@
 set -g fish_greeting
 set -gx TERM xterm-256color
 set -gx EDITOR nvim
-set -gx PAGER moar
+#set -gx PAGER moar
+set -gx PAGER ov
 set -g fish_key_bindings fish_vi_key_bindings
 set -gx MANPAGER "nvim +Man!"
 set -gx SHELL /usr/bin/fish
@@ -13,6 +14,7 @@ if status is-interactive
     fzf --fish | source
     zoxide init fish | source
     set -gx BAT_THEME "Catppuccin Mocha"
+    set -Ux FZF_DEFAULT_COMMAND "fd -H -E '.git'"
     set -Ux FZF_DEFAULT_OPTS "\
     --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
     --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
