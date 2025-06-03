b9524d558c94bc103ff06f22bf1682d4b3c7acfb
Jorge Acosta <Jorge.acosta@convergetp.com>
2025-06-03 17:12:48 -0400

    Update .local/bin/task_prompt.sh

--- private_dot_local/bin/executable_task_prompt.sh
+++ private_dot_local/bin/executable_task_prompt.sh
@@ -5,24 +5,18 @@ task_cmd="/usr/bin/task count rc.verbose: +READY"
 
 if [ $(${task_cmd} +TODAY) -gt "0" ]; then
   # Tasks due today
-  echo -e " 󰥌 "
+  echo -e "󰥌"
 elif [ $(${task_cmd} +TOMORROW) -gt "0" ]; then
   # Tasks due tomorrow
-  echo -e " 󰃰 "
+  echo -e "󰃰"
 elif [ $(${task_cmd} "urgency > 10") -gt "0" ]; then
   # Urgent tasks
-  echo -e " 󰨱 "
+  echo -e "󰨱"
 elif [ $(${task_cmd} +OVERDUE) -gt "0" ]; then
   # Overdue tasks
   # echo -e " 󰯆 "
-  echo -e "  "
+  echo -e ""
 else
-
-  if [[ $(task count rc.gc=off rc.verbose=nothing status:pending) -eq 0 ]]; then
-    echo ""
-  else
-    task count rc.gc=off rc.verbose=nothing
-  fi
-  # A count of all tasks
-  #${task_cmd}
+  echo ""
+  # echo -e "$(${task_cmd}) 󰥌 "
 fi
