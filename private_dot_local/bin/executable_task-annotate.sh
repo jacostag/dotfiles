2fff8320689168aa958e7f20f5123d9c93dfdd43
Jorge Acosta <Jorge.acosta@convergetp.com>
2025-06-03 17:15:20 -0400

    Update .local/bin/task-annotate.sh

--- private_dot_local/bin/executable_task-annotate.sh
+++ private_dot_local/bin/executable_task-annotate.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
+# Useful to add to a keymap on nvim to add a task with the editing file as annotation.
+# vim.keymap.set(
+#   "n",
+#   "<leader>ct",
+#   "<cmd>!~/.local/bin/task-annotate.sh '%:p' <CR>",
+#   { desc = "Add current file to a new taskwarrior task" }
+# )
 
 set -e
 
-task add +nvim "$@"
-ID=$(task +LATEST ids)
-task "$ID" annotate "$@"
+if ! command -v task &>/dev/null; then
+  error_exit "The 'task' command could not be found. Please ensure Taskwarrior is installed and in your PATH."
+fi
+
+/usr/bin/task add +nvim "$@"
+ID=$(/usr/bin/task +LATEST ids)
+/usr/bin/task "$ID" annotate "$@"
